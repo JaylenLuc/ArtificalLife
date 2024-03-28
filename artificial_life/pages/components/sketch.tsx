6ebe2ed6eb55065772801e098449ed0672d05ab2
jhluc <jhluc@uci.edu>
2024-03-27 17:36:13 -0700

    MVp

--- artificial_life/pages/components/sketch.tsx
+++ artificial_life/pages/components/sketch.tsx
@@ -7,16 +7,17 @@ const P5Sketch = () => {
          * CONSTANTS 
     ********************************************************/
     const renderRef = useRef(null);
-    const WIDTH_HEIGHT = 150
+    const WIDTH_HEIGHT = 100
     //const HEIGHT = 150
-    const SIZE = 2
+    const SIZE = 1
     const RENDER_THRESHOLD = .1
     const RGB_MIN_RANGE = 255 //min range
-    const ra = 21 //outer radius 
+    const ra = 9 //outer radius 
     const ri = ra/3 // inner radius 
     const ri_area = Math.PI * (ri*ri)
     const ra_area = (Math.PI * (ra*ra)) - (ri_area)
-    const alpha = 0.028
+    const alpha_n = 0.028
+    const alpha_m = 0.147
     //αn = 0.028, αm = 0.147
     //im just gonna assume there is only one alpha
     const d1 = 0.267
@@ -66,6 +67,12 @@ const P5Sketch = () => {
                 // 0 to 220 + MIN_RANGE
                 
                 let fill_value = (current_state * RGB_MIN_RANGE);
+
+                // if (row ==10 && col == 3){
+                //     console.log(fill_value)
+                //     console.log(current_state)
+                // }
+
                 //console.log(fill_value )
                 p.fill(fill_value,fill_value , fill_value)
                 p.circle(yPos, xPos , SIZE);  
@@ -89,18 +96,27 @@ const P5Sketch = () => {
         return Math.min(Math.max(value, lower_b), upper_b);
       };
 
-    const generalizeTransitionFunc = () => {
+    const generalizeTransitionFunc = ( ) => {
         for (let row = 0 ; row < WIDTH_HEIGHT; row ++){
             for (let col = 0 ; col < WIDTH_HEIGHT; col ++){
                 let m_n : Array<number> =  fillingIntegralN_M(row, col)
-                let new_value = transitionFunc_S(m_n[1], m_n[0])
-                cellsArray[row][col] += clamp(dt * new_value, 0, 1)
+                let new_value = transitionFunc_S(m_n[1], m_n[0]) // [-1,1]
+                //console.log(new_value)
+                new_value = dt *(2 * new_value -1)
+                //console.log("not clamped: ", new_value)
+                cellsArray[row][col] = clamp(cellsArray[row][col] + new_value, 0, 1 )
+                //console.log("clamped: ", cellsArray[row][col] )
+                // if (row ==10 && col == 3){
+                //     console.log( "rgb: ",cellsArray[row][col] * RGB_MIN_RANGE)
+                //     console.log("curren value : " ,cellsArray[row][col])
+                // }
                 //console.log(cellsArray[row][col])
 
             }
 
         }
         //console.log("here")
+
     }
 
 
@@ -121,7 +137,8 @@ const P5Sketch = () => {
         let c_col : number = _col
         let m : number = 0 
         let n : number = 0 
-
+        let M : number = 0 
+        let N : number = 0 
 
         for (let d_row = -(ra - 1) ; d_row < (ra - 1); ++d_row){ //iterate over the outer radius 
             let real_pos_row = emod(d_row + c_row, WIDTH_HEIGHT)
@@ -131,22 +148,24 @@ const P5Sketch = () => {
 
                 if (d_row*d_row + d_col* d_col  <= ri*ri){ //inner
                     m  += cellsArray[real_pos_row][real_pos_col]
+                    //M ++
 
                 }else if (d_row*d_row + d_col* d_col  <= ra*ra) {//outer
                     n += cellsArray[real_pos_row][real_pos_col]
-
+                    //N ++
                 }
                 
             }
         } 
         m /= ri_area
-
+        //m= clamp(m , 0 ,1 )
         n /= ra_area
+        //n = clamp(n , 0 ,1)
 
         //console.log("m = ", m, " ", "n = ", n)
 
         //console.log("tranisition value: ", transitionFunc_S(n,m))
-
+        //console.log(m, n)
         return [m,n] // inner, outer
     }
 
@@ -154,21 +173,21 @@ const P5Sketch = () => {
     /********************************************************
          * SIGMOIDS AND THE TRANSITION FUNCTION 
     ********************************************************/
-    const sigmoid1 = (x : number , a : number) => {
-        return 1/(1 + Math.exp(-(x-a) * 4/alpha))
+    const sigmoid1 = (x : number , a : number, alpha_val : number) => {
+        return 1/(1 + Math.exp(-(x-a) * 4/alpha_val))
     }
 
     const sigmoid2 = (x : number , a : number , b : number) => {
-        return sigmoid1(x,a) * (1- sigmoid1(x,b))
+        return sigmoid1(x,a, alpha_n) * (1- sigmoid1(x,b, alpha_n))
     }
 
     const sigmoidM = (x :number , y : number, m : number) => {
-        return x * ( 1 - sigmoid1(m, 0.5) ) + y * sigmoid1(m, 0.5)
+        return x * ( 1 - sigmoid1(m, 0.5,alpha_m) ) + (y * sigmoid1(m, 0.5,alpha_m))
     }
 
     const transitionFunc_S = (n : number, m : number ) => {
         //If the transition function in the discrete time-stepping scheme was sd(n, m) then the smooth one is s(n, m) = 2sd(n, m)−1.
-        return 2 * sigmoid2(n, sigmoidM(b1,d1,m), sigmoidM(b2,d2,m) ) - 1
+        return sigmoid2(n, sigmoidM(b1,d1,m), sigmoidM(b2,d2,m) )
     }
 
 
@@ -191,9 +210,8 @@ const P5Sketch = () => {
                 p.background(0,0,0);
 
                 generalizeTransitionFunc()
-                fillGrid(p);
                 
-
+                fillGrid(p);
             }
 
         })
