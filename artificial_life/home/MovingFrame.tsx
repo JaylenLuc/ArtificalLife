f1159a58564d4073e4e8af86738ae171b169ccc8
jhluc <jhluc@uci.edu>
2024-06-02 23:57:07 -0700

    movingFramer beta1

--- artificial_life/home/MovingFrame.tsx
+++ artificial_life/home/MovingFrame.tsx
@@ -1,23 +1,42 @@
+'use client'
 import React, { useState, useEffect } from 'react';
 import styles from "@/styles/Home.module.css";
 
-const generateRand = () => {
-  
-}
+
 
 const MovingFrame = () => {
-  const TOKEN1 = "@"
-  const TOKEN2 = "o"
-  const TOKEN3 = "+"
-  const TOKEN4 = "-"
-  const TOKEN5 = "."
-
-  var tokenArr : string[] = []
+  const tokensArr = ["~", "^", "-", "_"]
+  const token_length = tokensArr.length
+  const [currTokens, setCurrTokens] = useState([""]);
 
 
+  const generateRand = () => {
+    let newTokens = [];
+
+    for (let i = 0; i < 200; i++) {
+      if  (Math.random() > 0.5){
+        newTokens.push(tokensArr[Math.floor(Math.random() * token_length)]);
+      }
+    }
+    return newTokens;
+  }
+
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+      setCurrTokens(generateRand())   
+    }, 400);
+     
+    return () => clearInterval(interval); 
+  }, [])
+  
+  
   return (
     <div>
-        <h1 className = {styles.bigwords}>I am the matter of my own work</h1>
+      
+      <div className = {styles.smallwords}>{currTokens.map(token => <span>{token}</span>)}</div>
+      <h1 className = {styles.bigwords}>I am the matter of my own work</h1>
+      <div className = {styles.smallwords}>{currTokens.reverse().map(token => <span>{token}</span>)}</div>
     </div>
   );
 }
