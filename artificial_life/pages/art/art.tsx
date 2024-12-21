37748c057eb050effb33b0d0d9ea01d751c21e3b
JaylenLuc <JHLUC@UCI.EDU>
2024-12-20 20:55:28 -0800

    connecting to newnew45

---
+++ artificial_life/pages/art/art.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import styles from "./styles.module.css";
+
+export default function art() {
+   useEffect(() => {
+        return () => {
+            //console.log("cleaning up...");
+            
+            // comment this out to get 2 canvases and 2 draw() loops
+          };
+        
+
+
+    }, [ ])
+
+    
+return (
+
+    <div> 
+        <meta name="viewport" content="width=device-height"></meta>
+        <div className={styles.row}>
+            <div className={styles.col}>
+                <img src='/images/collage1.png' style={{'width':'100%'}}></img>
+            </div>
+            <div className={styles.col}>
+
+                <img src='/images/collage2.png' style={{'width':'100%'}}></img>
+            </div>
+        </div> 
+        <div className={styles.row}>
+            <div className={styles.col}>
+
+            </div>
+        </div>
+    </div>
+
+    )
+}
--- artificial_life/pages/index.tsx
+++ artificial_life/pages/index.tsx
@@ -78,6 +78,8 @@ function  Home()  {
           <br></br>
           <Link href="/genetic/genetic"><span className={styles.linkers}>Evolving Art</span></Link>
           <br></br>
+          <Link href="/art/art"><span className={styles.linkers}>World as Images</span></Link>
+          <br></br>
           <Link href="/userauth/signin"><span className={styles.linkers}>SIGN IN</span></Link>
           {/* <Link href= "https://the-biomes-atlas.vercel.app/"> </Link> THIS SECTION IS ABOUT MAPS AND ARCGIS AND ML*/}
           <br></br>
